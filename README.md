bd53b58ffbea3fcdf3fc461663d4d7e294e4106c
Adam Vogt <vogt.adam@gmail.com>
2025-09-15 12:38:20 -0400

    New features and cleanup
    - Add fux2 for 2nd species counterpoint. Use DuplicateRecordFields to
      avoid complicating the namespace (the user may end up having to add
      such extensions -- I show how to add one in the README)
    - Rename module from `Fux1` to `Fux` for consistency.
    - Move adapter code from the README song into `transposer`/TP
    - Remove unused dependencies (`hmm`, `huffman`): it doesn't look like
      these are going to be added any time soon.
    - Move extensions in Fux.hs to default-extensions in package.yaml

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# first species counterpoint for tidalcycles using sbv
+# counterpoint for tidalcycles using sbv
 
 ## installation
 Follow instructions at https://tidalcycles.org/, then add this library
@@ -13,14 +13,14 @@ cabal install --lib
 
 ```haskell
 -- juno-example.mp3
-import Fux1
+:set -XRecordWildCards
+
+import Fux
 do
     setcps 0.5
     let inst = mconcat [sound "juno" # djf (range 0.2 0.8 (slow 2 perlin)) # lpq (range 0.4 0.5 perlin),
             off 0.125 (|+ slow 2 (choose [2,7])) "arpy"]
-    let doh = 60
-    let nI xs = n $ cat $ pure . fromIntegral . subtract doh <$> xs
-    let asInt str = case parseTPat str of Right (TPat_Seq s) -> map (\(TPat_Atom _ a) -> doh + a) s
+    let TP { doh, nI, asInt } = transposer 60
     let gs = asInt "c d e g e e a4 c"
     xs <- cache (fux1 defaultFux1) gs
     let f q p = mconcat [fast 8 $ n c # struct b inst # gain 0.9,
--- lib/Fux.hs
+++ lib/Fux.hs
@@ -1,14 +1,8 @@
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-
-module Fux1 where
+module Fux where
 
 import Control.Lens hiding ((.>))
 import Control.Lens.Action
 import Control.Monad
-import Data.Compression.Huffman
 import Data.IORef
 import Data.List
 import qualified Data.Map as M
@@ -16,6 +10,9 @@ import Data.SBV
 import Data.SBV.Dynamic hiding (allSatWith)
 import Data.SBV.Internals
 import Debug.Trace
+import Sound.Tidal.Boot (cat, n)
+import Sound.Tidal.ParseBP
+import Sound.Tidal.Pattern
 import System.IO.Unsafe (unsafePerformIO)
 import Text.Read
 
@@ -39,11 +36,11 @@ data VoicePosition = Above | Below deriving (Show, Eq)
 
 -- Main function: generate all valid counterpoints
 fux1 :: Fux1 -> [Int] -> IO [[Int]]
-fux1 config cantus = do
+fux1 config@Fux1 {..} cantus = do
   if null (drop 2 cantus)
     then return []
     else do
-      solutions <- allSatWith defaultSMTCfg {allSatMaxModelCount = Just (nsol config)} $ do
+      solutions <- allSatWith defaultSMTCfg {allSatMaxModelCount = Just nsol} $ do
         counterpoint <- sIntegers $ map (("cp" ++) . show) (zipWith const [0 ..] cantus)
         applyFuxConstraints config cantus counterpoint
       print solutions
@@ -51,7 +48,7 @@ fux1 config cantus = do
 
 -- Apply all first species counterpoint constraints
 applyFuxConstraints :: Fux1 -> [Int] -> [SInteger] -> Symbolic ()
-applyFuxConstraints config (map fromIntegral -> gs) xs = do
+applyFuxConstraints config@Fux1 {..} (map fromIntegral -> gs) xs = do
   let lag x = zip x (drop 1 x)
 
   -- absolute difference between xs and gs
@@ -60,11 +57,11 @@ applyFuxConstraints config (map fromIntegral -> gs) xs = do
 
   constrain $
     sum [oneIf @Integer (x .== y) | (x, y) <- lag xs]
-      .<= fromIntegral (retriggers config)
+      .<= fromIntegral retriggers
 
   -- Range constraints
   do
-    let (minNote, maxNote) = allowedRange config & each %~ fromIntegral
+    let (minNote, maxNote) = allowedRange & each %~ fromIntegral
     constrain $ sAll (.>= minNote) xs
     constrain $ sAll (.<= maxNote) xs
 
@@ -72,8 +69,8 @@ applyFuxConstraints config (map fromIntegral -> gs) xs = do
   constrain $ sAll (`sNotElem` [1, 2, 6, 10, 11]) adxgs
 
   -- Voice crossing constraints
-  case voicePosition config of
-    _ | allowCrossing config -> return ()
+  case voicePosition of
+    _ | allowCrossing -> return ()
     Above -> constrain $ sAnd $ zipWith (.>=) xs gs
     Below -> constrain $ sAnd $ zipWith (.<=) xs gs
 
@@ -103,7 +100,110 @@ applyFuxConstraints config (map fromIntegral -> gs) xs = do
   adxgs ^!? _last . act \x -> constrain (x .== 0)
 
   case (reverse gs, reverse xs) of
-    (g : _, _ : xP : _) -> constrain $ applyCadence (cadenceType config) g xP
+    (g : _, _ : xP : _) -> constrain $ applyCadence cadenceType g xP
+    _ -> return ()
+
+data Fux2 = Fux2
+  { cadenceType :: CadenceType,
+    voicePosition :: VoicePosition, -- counterpoint above or below cantus firmus
+    allowCrossing :: Bool, -- allow voices to cross
+    allowedRange :: (Int, Int), -- MIDI range for counterpoint voice
+
+    -- | number of repeated notes in the voice
+    retriggers :: Int,
+    nsol :: Int
+  }
+
+fux2 :: Fux2 -> [(Int, Bool)] -> IO [[Int]]
+fux2 config@Fux2 {..} (unzip -> (cantus, duple)) = do
+  -- expand cantus to match counterpoint positions (1 or 2 notes per cantus)
+  let gs = concat [replicate (if d then 2 else 1) g | (g, d) <- zip cantus duple]
+  if length cantus < 3
+    then return []
+    else do
+      solutions <- allSatWith defaultSMTCfg {allSatMaxModelCount = Just nsol} $ do
+        counterpoint <- sIntegers $ map (("cp" ++) . show) [0 .. length gs - 1]
+        applyFux2Constraints config cantus duple counterpoint
+      print solutions
+      return $ extractSolutions solutions
+
+applyFux2Constraints :: Fux2 -> [Int] -> [Bool] -> [SInteger] -> Symbolic ()
+applyFux2Constraints config@Fux2 {..} cantus duple xs = do
+  let lag x = zip x (drop 1 x)
+      rep d = if d then 2 else 1
+      sizes = map rep duple
+      starts = init (scanl (+) 0 sizes) -- start index for each cantus note
+      gsBase = map fromIntegral cantus
+      gs = concat [replicate (rep d) g | (g, d) <- zip gsBase duple]
+      strongMask = concat [if d then [True, False] else [True] | d <- duple]
+      adxgs = [abs (x - g) `sEMod` 12 | (x, g) <- zip xs gs]
+      adxgsStrong = [a | (a, s) <- zip adxgs strongMask, s]
+      isConsonant ivl = ivl `sNotElem` [1, 2, 6, 10, 11]
+
+  -- retriggers (consecutive equal notes)
+  constrain $
+    sum [oneIf @Integer (x .== y) | (x, y) <- lag xs]
+      .<= fromIntegral retriggers
+
+  -- range
+  do
+    let (minNote, maxNote) = allowedRange & each %~ fromIntegral
+    constrain $ sAll (.>= minNote) xs
+    constrain $ sAll (.<= maxNote) xs
+
+  -- voice crossing
+  case voicePosition of
+    _ | allowCrossing -> return ()
+    Above -> constrain $ sAnd $ zipWith (.>=) xs gs
+    Below -> constrain $ sAnd $ zipWith (.<=) xs gs
+
+  -- no big jumps
+  constrain $ sAll (\(a, b) -> abs (a - b) .<= 12) (lag xs)
+
+  -- strong beats must be consonant
+  constrain $ sAll isConsonant [a | (a, s) <- zip adxgs strongMask, s]
+
+  -- weak beats: consonant OR (passing tone: step-step in same direction)
+  forM_ (zip3 [0 ..] starts sizes) $ \(j, s, sz) ->
+    when (sz == 2) $ do
+      let w = s + 1
+      if j + 1 < length starts
+        then do
+          let p = s
+              n = starts !! (j + 1)
+              dw1 = xs !! w - xs !! p
+              dw2 = xs !! n - xs !! w
+              passing = abs dw1 .== 1 .&& abs dw2 .== 1 .&& (dw1 * dw2 .> 0)
+          constrain $ isConsonant (adxgs !! w) .|| passing
+        else
+          -- last weak position: require consonance (no next strong to pass to)
+          constrain $ isConsonant (adxgs !! w)
+
+  -- no parallel 5ths/octaves across successive strong beats
+  constrain $
+    sAnd
+      [ i ./= k .|| j ./= k
+        | k <- [0, 7],
+          (i, j) <- lag adxgsStrong
+      ]
+
+  -- no 3 same imperfect consonances in a row on strong beats
+  constrain $
+    sAnd
+      [ sAny (./= m) ijk
+        | m <- [3, 4, 8, 9],
+          ijk <- map (take 3) $ takeWhile (not . null . drop 2) $ tails adxgsStrong
+      ]
+
+  -- start: perfect 5th or 8ve
+  adxgs ^!? _head . act \x -> constrain (x `sElem` [0, 7])
+
+  -- end: unison
+  adxgs ^!? _last . act \x -> constrain (x .== 0)
+
+  -- cadence relative to last cantus note, using penultimate cp note
+  case (reverse gs, reverse xs) of
+    (g : _, _ : xP : _) -> constrain $ applyCadence cadenceType g xP
     _ -> return ()
 
 applyCadence :: CadenceType -> SInteger -> SInteger -> SBool
@@ -138,9 +238,22 @@ defaultFux1 =
       nsol = 5000
     }
 
+defaultFux2 :: Fux2
+defaultFux2 =
+  Fux2
+    { cadenceType = Plagal,
+      voicePosition = Above,
+      allowCrossing = False,
+      retriggers = 2,
+      allowedRange = (60, 72), -- Middle C to C5
+      nsol = 5000
+    }
+
 -- Example usage
 exampleCantus :: [Int]
 exampleCantus = [60, 62, 64, 62, 57, 60] -- C-D-E-D-A-C
+
+exampleRhythm = cycle [False, True, False]
 
 -- has for 4608 valid counterpoints
 -- huffman
@@ -152,7 +265,7 @@ ngram n = takeWhile ((== n) . length) . map (take n) . tails
 runExample :: IO ()
 runExample = do
   putStrLn "Generating counterpoints for cantus firmus: C-D-E-D-C"
-  solutions <- fux1 defaultFux1 exampleCantus
+  solutions <- fux2 defaultFux2 (exampleCantus `zip` exampleRhythm)
 
   mapM_ print $ sortOn snd $ M.toList $ M.fromListWith (+) $ concat [map (,1) $ map relativize $ ngram 4 s | s <- solutions]
   putStrLn $ "Found " ++ show (length solutions) ++ " valid counterpoints:"
@@ -184,3 +297,11 @@ cache f g = do
       writeIORef xRef xn
       writeIORef gRef g
       return xn
+
+-- | return type for `transposer`
+data TP = TP {doh :: Int, nI :: [Int] -> ControlPattern, asInt :: String -> [Int]}
+
+transposer doh =
+  let nI xs = n $ cat $ pure . fromIntegral . subtract doh <$> xs
+      asInt str = case parseTPat str of Right (TPat_Seq s) -> map (\(TPat_Atom _ a) -> doh + a) s
+   in TP {..}
